28c044a9350e960987338ad8763a9fb24f42304f
Isabella Oliveira <ioliveira@iportsolutions.com.br>
2024-02-26 12:02:04 -0300

    adding cart module

--- ecomm/products/webpack.config.js
+++ ecomm/products/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 module.exports = {
   mode: "development",
@@ -6,6 +7,14 @@ module.exports = {
     port: 8081,
   },  
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'products', 
+      fileName: 'remoteEntry.js', 
+      exposes: {
+        // here we're renaming something , its an Alias 
+        './ProductsIndex': './src/index'
+      }
+    }),
     // take a look of wathever files are coming out of our webpack process
     // will add all the script tags automatically
     new HtmlWebpackPlugin({
